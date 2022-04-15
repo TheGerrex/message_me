f2306cc167814f7e9ce1259f9516698dcebe10a0
TheGerrex <gerrex95@gmail.com>
2022-04-14 20:51:18 -0500

    added users table and model and db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Paola", password: "123456")
+User.create(username: "HolasoyRene", password: "123456")
+User.create(username: "Maximiliano96", password: "123456")
+User.create(username: "PabloR", password: "123456")
+User.create(username: "Vanessa", password: "123456")
+User.create(username: "Fer85", password: "123456")
