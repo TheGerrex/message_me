729b574af91a6e34103450a4fbbf3d0751446f51
TheGerrex <gerrex95@gmail.com>
2022-04-15 00:29:09 -0500

    add auto-scrolling to chat window

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,17 @@ ActiveStorage.start()
 import '@doabit/semantic-ui-sass'
 
 
+window.scroll_bottom = function() {
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+}
+
 $(document).on("turbolinks:load", function() {
   $(".ui.dropdown").dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 }) 
 
